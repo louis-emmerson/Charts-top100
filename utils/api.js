a4ba5f6fba9e2cf14159d64c3656107f999eca12
emmo2000 <louis.emmerson2000@gmail.com>
2024-12-02 23:08:06 +0000

    removed logs

--- utils/api.js
+++ utils/api.js
@@ -5,7 +5,6 @@ const api = axios.create({baseURL:"https://itunes.apple.com"})
 function getTop100Albums(country = "us"){
     return api.get(`/${country}/rss/topalbums/limit=100/json`)
     .then(({data})=>{
-        console.log(data)
         return data.feed.entry
     })
 }
