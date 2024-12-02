8fd703b3866d486d69c065f8de3dfdccdfbe06bb
emmo2000 <louis.emmerson2000@gmail.com>
2024-12-02 23:07:50 +0000

    moved albums to albums page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import AlbumsList from "./components/AlbumsList"
+
 import Top100Albums from "./pages/Top100Albums"
 
 function App() {
   return (
     <section>
       <Top100Albums />
-      <AlbumsList />
+      
     </section>
   )
 }
--- src/pages/Top100Albums.jsx
+++ src/pages/Top100Albums.jsx
@@ -1,6 +1,9 @@
+import AlbumsList from "../components/AlbumsList"
+
 function Top100Albums(){
     return <>
     <h1>Top 100 Albums</h1>
+    <AlbumsList />
     </>
 }
 
