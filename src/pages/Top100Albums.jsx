7db215661e53746fe673ad0712c2ab088dd28eb5
emmo2000 <louis.emmerson2000@gmail.com>
2024-12-03 13:59:30 +0000

    search query text

--- src/pages/Top100Albums.jsx
+++ src/pages/Top100Albums.jsx
@@ -1,7 +1,11 @@
 import AlbumsList from "../components/AlbumsList"
 
-function Top100Albums(){
-    return <AlbumsList />
+function Top100Albums({searchInput}){
+    return( 
+    <section>
+        {searchInput?<h1 className=" text-center text-black font-medium text-2xl pt-4"> Search results for: {searchInput}</h1>:null}
+        <AlbumsList searchInput={searchInput} />
+    </section>)
 }
 
 export default Top100Albums
