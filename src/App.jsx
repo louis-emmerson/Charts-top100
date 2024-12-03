4ee95b4e871514976a4503157476c7a383ac3973
emmo2000 <louis.emmerson2000@gmail.com>
2024-12-03 11:21:08 +0000

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
+import Header from "./components/Header"
 import Top100Albums from "./pages/Top100Albums"
 
 function App() {
   return (
     <section>
+      <Header/>
       <Top100Albums />
-      
     </section>
   )
 }
