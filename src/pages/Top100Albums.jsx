dc976c7050bdfb630061e758afdada70cf09e789
emmo2000 <louis.emmerson2000@gmail.com>
2024-12-05 19:09:46 +0000

    updated padding when mobile menu visible

--- src/pages/Top100Albums.jsx
+++ src/pages/Top100Albums.jsx
@@ -6,7 +6,7 @@ function Top100Albums({nav}){
     const {searchInput} = useContext(SearchContext)
     return( 
     <section>
-        <div className={`${nav?"pt-40":"p-4"}`}>
+        <div className={`${nav?"pt-44":null}`}>
         {searchInput?<h1 className= {`text-center text-black font-medium text-2xl w-full`} > Search results for: {searchInput}</h1>:null}
         </div>
         <div className="md:flex items-center justify-center">
