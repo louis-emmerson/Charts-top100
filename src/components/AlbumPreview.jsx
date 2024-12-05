d7c43c1752bb257dfe922fd97b55cc7b3d878990
emmo2000 <louis.emmerson2000@gmail.com>
2024-12-05 17:37:33 +0000

    updated tracks list

--- src/components/AlbumPreview.jsx
+++ src/components/AlbumPreview.jsx
@@ -7,41 +7,49 @@ function AlbumPreview({ albumPreview, setToggleAlbumPreview }) {
   const albumTracks = albumPreview[1]
   const [currentTrack, setCurrentTrack] = useState(null)
 
-  
-    return (
+  return (
+    <div
+      className="fixed inset-0 z-30 flex items-center justify-center bg-black/60"
+      onClick={() => setToggleAlbumPreview(false)}
+    >
       <div
-        className="fixed inset-0 z-30 flex items-center justify-center bg-black/60"
-        onClick={() => setToggleAlbumPreview(false)}
+        className="bg-white rounded-lg shadow-lg max-w-md w-full p-6 relative"
+        onClick={(event) => event.stopPropagation()}
       >
-        <div
-          className="bg-white rounded-lg shadow-lg max-w-md w-full p-6 relative"
-          onClick={(event) => event.stopPropagation()}
-        >
-          <div className="mb-4 flex justify-center">
-            <img src={album["im:image"][0].label.replace("55x55", "250x250")} />
-          </div>
-          <div className="flex justify-center">
-            <h1 className="text-xl">{album["im:name"].label}</h1>
-          </div>
-          <div className="flex justify-center">
-            <h2 className="text-sm">{album["im:artist"].label}</h2>
-          </div>
-          {!albumTracks ? <ErrorAlert errorMsg="No Previews available"/> : (
-            <div>
-              <div className="mb-4">
-                <AudioPlayer currentTrack={currentTrack} />
-              </div>
+        <div className="mb-4 flex justify-center">
+          <img src={album["im:image"][0].label.replace("55x55", "250x250")} alt={`${album["im:name"].label} Album Artwork`} />
+        </div>
+        <div className="flex justify-center">
+          <h1 className="text-xl">{album["im:name"].label}</h1>
+        </div>
+        <div className="flex justify-center">
+          <h2 className="text-sm">{album["im:artist"].label}</h2>
+        </div>
+        {!albumTracks ? (
+          <ErrorAlert errorMsg="No Previews available" />
+        ) : (
+          <div>
+            <div className="mb-4">
+              <AudioPlayer currentTrack={currentTrack} />
+            </div>
 
-              <div className="space-y-2 max-h-36 overflow-y-auto">
-                {albumTracks.map((track, index) => (
-                  <div
-                    key={index}
-                    className="flex justify-between items-center bg-gray-50 p-2 rounded shadow"
+            <div className="space-y-2 max-h-36 overflow-y-auto">
+              <ul className="track-list">
+                {albumTracks.map((track) => (
+                  <li
+                    key={track.trackNumber}
+                    className="track-item flex items-center justify-between"
                   >
-                    <span className="text-sm pr-3">{track.trackNumber}</span>
-                    <span className="text-sm truncate">{track.trackName}</span>
+                    <div className="flex items-center">
+                      <span className="track-number text-sm pr-3">
+                        {track.trackNumber}
+                      </span>
+                      <span className="track-name text-sm truncate">
+                        {track.trackName}
+                      </span>
+                    </div>
                     <button
-                      className="bg-blue-500 text-white text-sm px-3 py-1 rounded "
+                      className="bg-blue-500 text-white text-base pr-4 pl-4 pt-1 pb-1 mb-1 rounded "
                       onClick={(event) => {
                         event.stopPropagation()
                         setCurrentTrack(track.previewUrl)
@@ -49,18 +57,19 @@ function AlbumPreview({ albumPreview, setToggleAlbumPreview }) {
                     >
                       Play
                     </button>
-                  </div>
+                  </li>
                 ))}
-              </div>
+              </ul>
             </div>
-          )}
-
-          <div className="flex justify-center text-center pt-2">
-            <h2 className="text-xs">{album.rights.label}</h2>
           </div>
+        )}
+
+        <div className="flex justify-center text-center pt-2">
+          <h2 className="text-xs">{album.rights.label}</h2>
         </div>
       </div>
-    )
+    </div>
+  )
 }
 
 export default AlbumPreview
